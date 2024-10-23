79122006d3cbaf434e7c8e8142a77992e938fdc9
Abhishek Ganvir <abhishekganvir09@gmail.com>
2024-10-23 19:42:59 +0530

    dom project done

---
+++ script.js
@@ -0,0 +1,13 @@
+//console.log("Abhishek");
+let xyz= document.querySelector(".one")
+
+xyz.style.height="200px"
+xyz.style.backgroundColor ="yellow"
+xyz.innerText ="Hello, I'm a div!"
+xyz.style.width="300px"
+xyz.style.fontWeight = "bold"
+xyz.style.color ="blue"
+xyz.style.fontSize="19px"
+//xyz.style.padding-top ="5px"
+xyz.style.margin="50px , 20px"
+xyz.style.padding="15px"
